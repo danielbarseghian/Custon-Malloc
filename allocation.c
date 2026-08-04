1e01c758ab1b0ae3352327716280b9aa199fba0b
Daniel BARSEGHIAN <barseghian.daniel@gmail.com>
2026-08-05 00:01:48 +0200

    Structuring the code

--- allocation.c
+++ allocation.c
@@ -1,6 +1,21 @@
 #include "allocation.h"
 
-int main(void)
+struct header {
+    size_t size; // located at address + 0 and adress + 2 + size
+    unsigned int used; // located at adress + 1 (1 = free | 2 = used)
+} header;
+
+void *malloc(size_t size)
 {
-    printf("Heloo\n");
+
+}
+
+void free(void *ptnr)
+{
+
+}
+
+void *get_free_block(size_t size)
+{
+
 }
--- allocation.h
+++ allocation.h
@@ -1,1 +1,5 @@
 #include <stdio.h>
+
+void *malloc(size_t size);
+void free(void *ptnr);
+void *get_free_block(size_t size);
